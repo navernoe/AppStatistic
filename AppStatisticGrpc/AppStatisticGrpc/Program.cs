9e93a50f1b267b8247a56118c98bfa504268573e
Violetta Gl <me@mail.ru>
2021-03-08 18:13:05 +0300

    AppStatisticGrpc: comment on ConfigureKestrel

--- AppStatisticGrpc/AppStatisticGrpc/Program.cs
+++ AppStatisticGrpc/AppStatisticGrpc/Program.cs
@@ -11,18 +11,19 @@ namespace AppStatisticGrpc
             CreateHostBuilder(args).Build().Run();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
+        // Additional configuration is required to successfully run gRPC on macOS (Windows 7).
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    /*
                     webBuilder.ConfigureKestrel(options =>
                     {
                         options.ListenLocalhost(5002, o => o.Protocols = HttpProtocols.Http2);
                     });
-                    
+                    */
                 });
     }
 }
